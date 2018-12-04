6bc894d77ec2cf2ef69a3ae6a9184200a78e2036
tpmccrary <42851426+tpmccrary@users.noreply.github.com>
2018-12-04 02:34:20 -0700

    Update MainRunner.py

--- MainRunner.py
+++ MainRunner.py
@@ -1,31 +1,80 @@
+# **********************************************************************************************************************
+# NAME: Timothy P. McCrary
+# CLASS: CS 2302
+# LAB 6 OPTION A
+# INSTRUCTOR: Diego Aguirre
+# TA: Manoj Pravaka Saha
+# DATE: 12/3/2018
+# PURPOSE: To use and manipulate a Graph data structure.
+# **********************************************************************************************************************
+
 import DisjointSetForest
 import Graph
 import TopologicalSort
+import KruskalsAlgorithm
 
 
 def main():
-    graph = Graph.GraphAL(3, True)
-    graph.add_edge(0, 1)
-    # graph.add_edge(0, 2)
-    graph.add_edge(1, 2)
-    graph.add_edge(2, 0)
+    """
+    Main function/Runner function.
+    :return:
+    """
+    # ======================
+    # TOPOLOGICAL SORT TESTS
+    # ======================
+    print('__________Test 1 - Acyclic graph__________')
+    graph = Graph.GraphAL(9, True)
 
-    print(graph.adj_list)
+    graph.add_edge(0, 1)
+    graph.add_edge(2, 1)
+    graph.add_edge(3, 2)
+    graph.add_edge(3, 6)
+    graph.add_edge(4, 0)
+    graph.add_edge(4, 1)
+    graph.add_edge(5, 1)
+    graph.add_edge(5, 2)
+    graph.add_edge(5, 4)
+    graph.add_edge(5, 7)
+    graph.add_edge(6, 2)
+    graph.add_edge(6, 5)
+    graph.add_edge(6, 8)
+    graph.add_edge(7, 4)
+    graph.add_edge(8, 5)
+    graph.add_edge(8, 7)
+
+    print('Topological sort for this graph:', TopologicalSort.topological_sort(graph))
+
+    # ========================
+    # KRUSKALS ALGORITHM TESTS
+    #=========================
+
+    print('Test 1 - Kruskals Algorithm')
+
+    graph = Graph.GraphAL(8, False)
+
+    graph.add_edge(0, 1, 2)
+    graph.add_edge(0, 4, 3)
+    graph.add_edge(1, 2, 9)
+    graph.add_edge(1, 4, 6)
+    graph.add_edge(1, 5, 8)
+    graph.add_edge(2, 3, 10)
+    graph.add_edge(2, 5, 7)
+    graph.add_edge(2, 6, 15)
+    graph.add_edge(3, 6, 16)
+    graph.add_edge(4, 5, 1)
+    graph.add_edge(4, 7, 4)
+    graph.add_edge(5, 6, 12)
+    graph.add_edge(5, 7, 11)
+    graph.add_edge(6, 7, 13)
+
     for i in range(len(graph.adj_list)):
         temp = graph.adj_list[i]
+        print('Vertex:', i)
         while temp is not None:
-            print('Index:', i, '\tItem =', temp.item)
+            print(temp.item)
             temp = temp.next
 
-    # print(TopologicalSort.topological_sort(graph))
-
-    print(graph.get_vertex_in_degree(2))
-    print(graph.get_vertex_in_degree(0))
-
-    print(graph.is_there_a_two_vertex_cycle())
-
-
-
+    print('Kruskals Algorithm for this graph:', KruskalsAlgorithm.kruskals_algorithm(graph))
 
 
 
